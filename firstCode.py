b4523f1eee9c87dc6b6a67ebfb5b8fd65ac79473
Mariah-N <mariahnuzzo3@gmail.com>
2025-06-17 18:21:55 +0000

    Added response options to firstCode

--- firstCode.py
+++ firstCode.py
@@ -2,3 +2,6 @@ print("hello world")  # test with print statement
 print('Test that changes are detected in commit')
 print('change from repository')
 #note need to 'git pull' in codespace to get the latest changes from the repository
+
+response = input("Enter your name: ")  # wait for user input
+print(f"Hello {response}")  # greet the user
